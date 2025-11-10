9962453d216879963045693735c9576b251b36d5
Adam Sifounakis <asifouna@mathworks.com>
2025-11-09 23:41:53 -0500

    Addressed reviewer feedback

--- tests/testSimpleSort.m
+++ tests/testSimpleSort.m
@@ -1,11 +1,11 @@
 classdef testSimpleSort < matlab.unittest.TestCase
     
     properties (TestParameter)
-        inputValues = struct( ...
+        inputValue = struct( ...
             ScalarPositive = 5, ...
             ScalarNegative = -7, ...
             ScalarZero = 0, ...
-            VectorLength2 = randi(100,[20,1]), ...
+            VectorLength2 = randi(100,[2,1]), ...
             VectorLength10 = randi(100,[10,1]), ...
             VectorLength100 = randi(100,[100,1]), ...
             Empty = []);
@@ -13,7 +13,7 @@ classdef testSimpleSort < matlab.unittest.TestCase
 
     methods (Test)
 
-        function test_simpleSort_ClassicTest(testCase)
+        function testSimpleSort_ExampleBasedTest(testCase)
             % This is a classic test using defined inputs and expected
             % outputs
 
@@ -28,33 +28,35 @@ classdef testSimpleSort < matlab.unittest.TestCase
 
             % Verify results
             testCase.verifyEqual(actualOut,expectedOut);
-
         end
 
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-        % Uncomment the following tests and add code to complete the test %
-        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-        % function test_simpleSort_PropertyBasedTest(testCase, inputValues)
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % Below is a property-based test that is missing the logic to %
+        % test the properties of of the result. Uncomment and update  %
+        %                  the code to make it pass.                  %
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+        % function testSimpleSort_PropertyBasedTest(testCase, inputValue)
         %     % Create a property-based test for simpleSort
         % 
         %     % Instead of testing specific input and output values like we
         %     % did above, property-based testing checks that the
-        %     % "properties" (i.e., the defining characterstics) of the
-        %     % result that must always be true are actually satisfied.
+        %     % "properties" (i.e., the defining characteristics) of the
+        %     % result are satisfied.
+        % 
+        %     % A major benefit of property-based testing is that you don't
+        %     % don't need to define any specific expected values. We
+        %     % expect that the properties of the solution are true, so the
+        %     % exact values are less important (or not important at all).
         % 
         %     % For simpleSort, the properties of the algorithm are:
-        %     %   - Output has the same number of elements as the input
         %     %   - Output is sorted in ascending order
-        % 
-        %     % Another nice benefit of property-based testing is that you
-        %     % don't need to specify any expected values. The expecation is
-        %     % that the properties of the solution are true, so the specific
-        %     % values are less important (or not important at all).
+        %     %   - Output has the same number of elements as the input
         % 
         % 
         %     % Exercise function
-        %     output = simpleSort(inputValues);
+        %     output = simpleSort(inputValue);
         % 
         % 
         %     % Compute and verify properties of the result. Be sure to check
@@ -64,18 +66,34 @@ classdef testSimpleSort < matlab.unittest.TestCase
         %     % e.g., testCase.verifyTrue( all(output > 0) )
         % 
         % 
-        %     %%%%%%%%%%%%%%%%%%%%
-        %     % MODIFY CODE HERE %
-        %     %%%%%%%%%%%%%%%%%%%%
-        %     hasSameNumberOfElements = false;
-        %     isSortedAscending = false;
+        %     % Perform logic to check each property of the result
+        %     isSortedAscending = checkIsSortedAscending(output);
+        %     hasSameNumberOfElements = checkHasSameNumberOfElements(inputValue,output);
         % 
         % 
         %     % Verify properties of result are satisfied
-        %     testCase.verifyTrue(hasSameNumberOfElements);
         %     testCase.verifyTrue(isSortedAscending);
-        % 
+        %     testCase.verifyTrue(hasSameNumberOfElements);
         % end
 
     end
+end
+
+function isSortedAscending = checkIsSortedAscending(output)
+% HOMEWORK: Add logic to check that the output is sorted in ascending order
+%           Note: The output should be a logical true/false
+
+% ADD CODE HERE
+
+
+end
+
+function hasSameNumberOfElements = checkHasSameNumberOfElements(inputValue,output)
+% HOMEWORK: Add logic to check that the output has the same number of
+%           elements as the input
+%           Note: The output should be a logical true/false
+
+% ADD CODE HERE
+
+
 end
